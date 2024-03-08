d8d7ae4caa7d758126acfa177a0eb5cfaea44098
s28044 <s28044@pjwstk.edu.pl>
2024-03-08 15:23:22 +0100

    cokolwiek

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine(GetAVG([1.0,2.0,3.0,4.0,5.0]));
+Console.WriteLine(GetMax([1,2,3,4,5,6,7]));
+static double GetAVG(double[] tab)
+{
+    double sum = 0;
+    foreach (var VARIABLE in tab)
+    {
+        sum += VARIABLE;
+    }
+    return sum / tab.Length;
+
+ 
+
+}
+static int GetMax(int[] arr)
+{
+    int max = 0;
+
+    foreach (var liczba in arr)
+    {
+        if (max<liczba)
+        {
+            max = liczba;
+        }   
+    }
+
+    return max;
+}
